44cdca9126241823f8ca20195a14b6a12a1b01f5
Adrianodev00play <160899241+Adrianodev00play@users.noreply.github.com>
2026-04-13 21:25:43 -0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import os
-os.system("cls")
-km=float(input("Digite a quantidade de km percorridos: "))
-dia=int(input("Digite a quantos dias: "))
-
-valor=(dia*60)+(km*0.15)
-
-print("Você irá pagar ",dia*60+km*0.15," reais")
