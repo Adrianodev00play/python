edf4f56982217a88dffd988a9e63a910743c0698
Adrianodev00play <160899241+Adrianodev00play@users.noreply.github.com>
2026-04-13 21:25:32 -0300

    Delete idade.py

--- idade.py
+++
@@ -1,5 +0,0 @@
-#código para verificar se é maior de idade.
-idade = int(input("Informe sua idade: "))
-if idade >= 18:
-    print("Você é maior de idade e tem",idade,"anos.")
-else: print("Você é menor de idade.")
